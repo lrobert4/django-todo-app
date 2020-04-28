48d452564e430e9ef6b7b7b41527da439195453e
leer <ladesigns90@gmail.com>
2020-04-28 18:55:41 -0400

    Created forms using the built-in form modules in django.

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import todo_list
+from .views import todo_list, todo_detail, todo_create
 
 app_name = 'todos'
 
 urlpatterns = [
-    path('', todo_list)
+    path('', todo_list),
+    path('create/', todo_create),
+    path('<id>/', todo_detail),
+    
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TodoForm
 from .models import Todo
 
 def todo_list(request):
@@ -10,3 +11,20 @@ def todo_list(request):
     }
     return render(request, "todo/todo_list.html", context)
 
+# CRUD - Create, Retrieve, Update, Delete, List
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        "todo": todo
+    }
+    return render(request, "todo/todo_detail.html", context)
+
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "todo/todo_create.html", context)
